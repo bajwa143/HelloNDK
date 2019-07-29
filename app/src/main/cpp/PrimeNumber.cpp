4e812c901541c1d5aefc6f4528bdd56c4cbc81d3
Saqib <hello_friends_1@yahoo.com>
2019-07-29 15:13:23 +0500

    Android Logging added using android/log.h library. It is just for sake to analyzing prime function and practicing logging in NDK using c/c++.

--- app/src/main/cpp/PrimeNumber.cpp
+++ app/src/main/cpp/PrimeNumber.cpp
@@ -1,5 +1,6 @@
 
 #include "PrimeNumber.h"
+#include <android/log.h>
 
 PrimeNumber::PrimeNumber(int number) {
     this->number = number;
@@ -11,6 +12,7 @@ int PrimeNumber::getNumber() {
 
 bool PrimeNumber::isPrime() {
     if (number <= 1) {
+        __android_log_write(ANDROID_LOG_INFO,"Number Value","Value is less than 1");
         return false;
     }
 
@@ -21,11 +23,13 @@ bool PrimeNumber::isPrime() {
     bool ans;
     for (int i = 2; i <= number / 2; i++) {
         if (number % i == 0) {
+            __android_log_print(ANDROID_LOG_INFO,"Number Value","False return at i value %d",i);
             return false;
         } else {
             ans = true;
         }
     }
+    __android_log_write(ANDROID_LOG_INFO,"Number Value","Reached to end of loop, returning true");
     return ans;
 }
 
